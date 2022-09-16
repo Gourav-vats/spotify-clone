7b0e65b7e5e709df5364714db85f741631701089
Gourav Vats <gourav4142@gmail.com>
2022-09-16 17:23:51 +0530

    middleware added to protect restricted routes

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -7,7 +7,7 @@ import {
   HeartIcon,
   RssIcon,
 } from "@heroicons/react/outline";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 
 const Sidebar = () => {
   const { data: session } = useSession();
@@ -15,6 +15,7 @@ const Sidebar = () => {
   return (
     <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
       <div className="space-y-4">
+        <button onClick={() => signOut()}>Log out</button>
         <button className="flex items-center space-x-2 hover:text-white">
           <HomeIcon className="h-5 w-5" />
           <p>Home</p>
---
+++ middleware.js
@@ -0,0 +1,21 @@
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+
+export async function middleware(req) {
+  // Token will exist if user is logged in
+  const token = await getToken({ req });
+  const { pathname } = req.nextUrl;
+
+  if (pathname.startsWith("/_next")) return NextResponse.next();
+  // Allow the requests if the following is true..
+  // 1. Its a request for getting a next-auth session
+  // 2. Token exists
+
+  if (pathname.includes("/api/auth") || token) return NextResponse.next();
+
+  // Redirect user to login page if user does'nt have a token AND requesting a protected route
+
+  if (!token && pathname !== "/login") {
+    return NextResponse.redirect("http://localhost:3000/login");
+  }
+}
