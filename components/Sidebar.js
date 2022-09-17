50cfa34d9735283c2a0a68b344295fc2318004a3
Gourav Vats <gourav4142@gmail.com>
2022-09-17 11:14:07 +0530

    user playlists rendered into sidebar, spotify hook created

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSession, signOut } from "next-auth/react";
+import useSpotify from "../hooks/useSpotify";
 import {
   HomeIcon,
   SearchIcon,
@@ -7,40 +9,61 @@ import {
   HeartIcon,
   RssIcon,
 } from "@heroicons/react/outline";
-import { signOut } from "next-auth/react";
 
 const Sidebar = () => {
+  const spotifyApi = useSpotify();
+  const { data: session } = useSession();
+  const [playlists, setPlaylists] = useState();
+
+  useEffect(() => {
+    if (spotifyApi.getAccessToken()) {
+      spotifyApi.getUserPlaylists().then((data) => {
+        setPlaylists(data.body.items);
+      });
+    }
+  }, [session, spotifyApi]);
+
   return (
     <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll h-screen scrollbar-hide">
-      <div className="space-y-4">
-        <button onClick={() => signOut()}>Log out</button>
-        <button className="flex items-center space-x-2 hover:text-white">
-          <HomeIcon className="h-5 w-5" />
-          <p>Home</p>
-        </button>
-        <button className="flex items-center space-x-2 hover:text-white">
-          <SearchIcon className="h-5 w-5" />
-          <p>Search</p>
-        </button>
-        <button className="flex items-center space-x-2 hover:text-white">
-          <LibraryIcon className="h-5 w-5" />
-          <p>Your Library</p>
-        </button>
-        <hr />
-        <button className="flex items-center space-x-2 hover:text-white">
-          <PlusCircleIcon className="h-5 w-5" />
-          <p>Create Playlist</p>
-        </button>
-        <button className="flex items-center space-x-2 hover:text-white">
-          <HeartIcon className="h-5 w-5" />
-          <p>Liked Songs</p>
-        </button>
-        <button className="flex items-center space-x-2 hover:text-white">
-          <RssIcon className="h-5 w-5" />
-          <p>Your episodes</p>
-        </button>
-        <hr />
-        <p className="hover:text-white cursor-pointer">Your Playlists</p>
+      <div className="space-y-4 divide-y">
+        <div className="space-y-4">
+          <button onClick={() => signOut()}>Log out</button>
+          <button className="flex items-center space-x-2 hover:text-white">
+            <HomeIcon className="h-5 w-5" />
+            <p>Home</p>
+          </button>
+          <button className="flex items-center space-x-2 hover:text-white">
+            <SearchIcon className="h-5 w-5" />
+            <p>Search</p>
+          </button>
+          <button className="flex items-center space-x-2 hover:text-white">
+            <LibraryIcon className="h-5 w-5" />
+            <p>Your Library</p>
+          </button>
+        </div>
+        {/* <hr className="text-black" /> */}
+        <div className="space-y-4">
+          <button className="flex items-center space-x-2 hover:text-white">
+            <PlusCircleIcon className="h-5 w-5" />
+            <p>Create Playlist</p>
+          </button>
+          <button className="flex items-center space-x-2 hover:text-white">
+            <HeartIcon className="h-5 w-5" />
+            <p>Liked Songs</p>
+          </button>
+          <button className="flex items-center space-x-2 hover:text-white">
+            <RssIcon className="h-5 w-5" />
+            <p>Your episodes</p>
+          </button>
+        </div>
+        {/* <hr /> */}
+        <div className="space-y-4">
+          {playlists.map((playlist) => (
+            <p key={playlist.id} className="hover:text-white cursor-pointer">
+              {playlist.name}
+            </p>
+          ))}
+        </div>
       </div>
     </div>
   );
