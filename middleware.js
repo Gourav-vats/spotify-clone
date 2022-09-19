774caca3e9f89aabbe46f310b3a14cf76b21dd3b
Gourav Vats <gourav4142@gmail.com>
2022-09-20 00:41:38 +0530

    middleware bug fixed

--- middleware.js
+++ middleware.js
@@ -6,6 +6,8 @@ export async function middleware(req) {
   const token = await getToken({ req });
   const { pathname } = req.nextUrl;
 
+  const url = req.nextUrl.clone();
+
   if (pathname.startsWith("/_next")) return NextResponse.next();
   // Allow the requests if the following is true..
   // 1. Its a request for getting a next-auth session
@@ -16,6 +18,7 @@ export async function middleware(req) {
   // Redirect user to login page if user does'nt have a token AND requesting a protected route
 
   if (!token && pathname !== "/login") {
-    return NextResponse.redirect("/login");
+    url.pathname = "/login";
+    return NextResponse.rewrite(url);
   }
 }
